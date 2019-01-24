4c1652fced575b55fddf8cb0a5d428a8ad4a9380
ROBERT HELLYER <ROBHELLY@uat.edu>
2019-01-24 12:41:08 -0700

    Add project files.

---
+++ SkyNet/Military.cpp
@@ -0,0 +1,15 @@
+//
+#include <iostream>
+//
+using namespace std;
+
+//code block where all the code goes.
+int main ()
+{
+
+	//Welcomes user
+	cout << "Test" << endl;
+	//pause screen for user
+	system("pause");
+
+}
